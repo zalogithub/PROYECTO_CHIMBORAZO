88dab8ea33e66b8e45c9d6f5afcd4991cbb7c434
zalogithub <gallauca@espoch.edu.ec>
2013-01-11 00:36:28 -0500

    Subo el sitio completo

---
+++ h.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>Formulario a generar en pdf</title>
+<h1>VAMOS A GENERAR NUESTRO PRIMER PDF</h1>
+</head>
+<body>
+<form action="generador.php" method="post">
+Nombre : <input name="nombre" type="text"><br>
+Direccion: <input name="direccion" type="text">
+Telefono : <input name="tel" type="text">
+<input type="submit" name="Generar">
+
+</form>
+</body>
+
+</html>
